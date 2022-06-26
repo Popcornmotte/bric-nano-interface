4e3e07e26772cd65caa144b24b5d478d998f4605
Popcornmotte <popcornmotte@gmx.net>
2022-06-26 19:59:58 +0200

    Edit test app

--- c/obj/KEY_APP.c
+++ c/obj/KEY_APP.c
@@ -16,45 +16,47 @@
 
 void Draw_Init(void)
 {
-    LCD_Clear(WHITE);
+    LCD_Clear(BLACK);
 
-    /* Press */
-    GUI_DrawRectangle(40, 60, 60, 80, RED, DRAW_EMPTY, DOT_PIXEL_DFT);
-    GUI_DisString_EN(43, 60, "P", &Font24, GUI_BACKGROUND, BLUE);
+    GUI_DisString_EN(10, 10, "Main Menu", &Font16, GUI_BACKGROUND, WHITE);
 
-    /* Left */
-    GUI_DrawRectangle(20, 60, 40, 80, RED, DRAW_EMPTY, DOT_PIXEL_DFT);
-    GUI_DisString_EN(23, 60, "L", &Font24, GUI_BACKGROUND, BLUE);
-
-    /* Right */
-    GUI_DrawRectangle(60, 60, 80, 80, RED, DRAW_EMPTY, DOT_PIXEL_DFT);
-    GUI_DisString_EN(63, 60, "R", &Font24, GUI_BACKGROUND, BLUE);
-
-    /* Up */
-    GUI_DrawRectangle(40, 40, 60, 60, RED, DRAW_EMPTY, DOT_PIXEL_DFT);
-    GUI_DisString_EN(43, 40, "U", &Font24, GUI_BACKGROUND, BLUE);
-
-    /* Down */
-    GUI_DrawRectangle(40, 80, 60, 100, RED, DRAW_EMPTY, DOT_PIXEL_DFT);
-    GUI_DisString_EN(43, 80, "D", &Font24, GUI_BACKGROUND, BLUE);
-
-    /* Key1 */
-    GUI_DrawRectangle(95, 40, 120, 60, RED, DRAW_EMPTY, DOT_PIXEL_DFT);
-    GUI_DisString_EN(98, 43, "K1", &Font16, GUI_BACKGROUND, BLUE);
-
-    /* Key2	*/
-    GUI_DrawRectangle(95, 60, 120, 80, RED, DRAW_EMPTY, DOT_PIXEL_DFT);
-    GUI_DisString_EN(98, 63, "K2", &Font16, GUI_BACKGROUND, BLUE);
-
-    /* Key3 */
-    GUI_DrawRectangle(95, 80, 120, 100, RED, DRAW_EMPTY, DOT_PIXEL_DFT);
-    GUI_DisString_EN(98, 83, "K3", &Font16, GUI_BACKGROUND, BLUE);
+//    /* Press */
+//    GUI_DrawRectangle(40, 60, 60, 80, RED, DRAW_EMPTY, DOT_PIXEL_DFT);
+//    GUI_DisString_EN(43, 60, "P", &Font24, GUI_BACKGROUND, BLUE);
+//
+//    /* Left */
+//    GUI_DrawRectangle(20, 60, 40, 80, RED, DRAW_EMPTY, DOT_PIXEL_DFT);
+//    GUI_DisString_EN(23, 60, "L", &Font24, GUI_BACKGROUND, BLUE);
+//
+//    /* Right */
+//    GUI_DrawRectangle(60, 60, 80, 80, RED, DRAW_EMPTY, DOT_PIXEL_DFT);
+//    GUI_DisString_EN(63, 60, "R", &Font24, GUI_BACKGROUND, BLUE);
+//
+//    /* Up */
+//    GUI_DrawRectangle(40, 40, 60, 60, RED, DRAW_EMPTY, DOT_PIXEL_DFT);
+//    GUI_DisString_EN(43, 40, "U", &Font24, GUI_BACKGROUND, BLUE);
+//
+//    /* Down */
+//    GUI_DrawRectangle(40, 80, 60, 100, RED, DRAW_EMPTY, DOT_PIXEL_DFT);
+//    GUI_DisString_EN(43, 80, "D", &Font24, GUI_BACKGROUND, BLUE);
+//
+//    /* Key1 */
+//    GUI_DrawRectangle(95, 40, 120, 60, RED, DRAW_EMPTY, DOT_PIXEL_DFT);
+//    GUI_DisString_EN(98, 43, "K1", &Font16, GUI_BACKGROUND, BLUE);
+//
+//    /* Key2	*/
+//    GUI_DrawRectangle(95, 60, 120, 80, RED, DRAW_EMPTY, DOT_PIXEL_DFT);
+//    GUI_DisString_EN(98, 63, "K2", &Font16, GUI_BACKGROUND, BLUE);
+//
+//    /* Key3 */
+//    GUI_DrawRectangle(95, 80, 120, 100, RED, DRAW_EMPTY, DOT_PIXEL_DFT);
+//    GUI_DisString_EN(98, 83, "K3", &Font16, GUI_BACKGROUND, BLUE);
 }
 
 void KEY_Listen(void)
 {
     Draw_Init();
-    for(;;) {
+    for(;;) {/*
         if(GET_KEY_UP == 0) {
             while(GET_KEY_UP == 0) {
                 GUI_DrawRectangle(40, 40, 60, 60, BLUE, DRAW_FULL, DOT_PIXEL_DFT);
@@ -126,6 +128,6 @@ void KEY_Listen(void)
             GUI_DrawRectangle(95, 80, 120, 100, WHITE, DRAW_FULL, DOT_PIXEL_DFT);
             GUI_DrawRectangle(95, 80, 120, 100, RED, DRAW_EMPTY, DOT_PIXEL_DFT);
             GUI_DisString_EN(98, 83, "K3", &Font16, GUI_BACKGROUND, BLUE);
-        }
+        }*/
     }
 }
